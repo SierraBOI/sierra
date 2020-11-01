4ecc3b7870534e7b6e80df0cbb644faa32376926
SierraBOI <thijsvgrinsven.xd@hotmail.com>
2020-11-01 02:16:05 +0100

    v2

--- BlinkWithoutDelayNewTry.ino
+++ BlinkWithoutDelayNewTry.ino
@@ -1,12 +1,18 @@
 //Global Variables
 const byte LED=12; 
 int commandState;
+int ledTime = 0;
+unsigned long interval = 1100;
+unsigned long previousMillis = 0;
 unsigned long buttonPushedMillis; // when button was released
 unsigned long ledTurnedOnAt; // when led was turned on
-unsigned long turnOnDelay = 100; // wait to turn on LED
+unsigned long ledTurnedOffOne;
+unsigned long turnOnDelay = 1000; // wait to turn on LED
 unsigned long turnOffDelay = 1000; // turn off LED after this time
+unsigned long inBetweenDelay = 1200; // turn the led on a few times
 bool ledReady = false; // flag for when button is let go
 bool ledState = false; // for LED is on or not.
+bool nextRound = false;
  
 void setup() {
  pinMode(12, OUTPUT);
@@ -17,21 +23,24 @@ void setup() {
 void loop() {
  // get the time at the start of this loop()
   unsigned long currentMillis = millis(); 
+ 
   commandState = analogRead(A0);
   Serial.println(commandState);
  
  // check the button
  if (commandState == 959) {
   // update the time when button was pushed
-  buttonPushedMillis = currentMillis;
+  
+   buttonPushedMillis = currentMillis;
   ledReady = true;
+   
  }
   
  // make sure this code isn't checked until after button has been let go
  if (ledReady) {
    //this is typical millis code here:
    Serial.println("start"); 
-   if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnDelay) {
+  
      // okay, enough time has passed since the button was let go.
      digitalWrite(LED, HIGH);
      // setup our next "state"
@@ -40,15 +49,29 @@ void loop() {
      ledTurnedOnAt = currentMillis;
      // wait for next button press
      ledReady = false;
-   }
+     Serial.println("false");
+   
  }
   
  // see if we are watching for the time to turn off LED
  if (ledState) {
+   
    // okay, led on, check for now long
    if ((unsigned long)(currentMillis - ledTurnedOnAt) >= turnOffDelay) {
+     Serial.println("hey turnt off");
      ledState = false;
      digitalWrite(LED, LOW);
+     ledTurnedOffOne = currentMillis;
+       
+   }
+   
+   if((unsigned long)(currentMillis - ledTurnedOffOne) >= interval) {
+     Serial.println("GO AGAIN");
+     ledReady = false;
+   
+
+   
+    }
+   
    }
  }
-}
